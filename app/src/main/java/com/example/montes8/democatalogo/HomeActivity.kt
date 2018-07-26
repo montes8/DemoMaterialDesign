12f84f70e36955f695b053a3faa65144acf92bf6
montes8 <emontesinos812@gmail.com>
2018-07-25 20:53:21 -0500

    diseñando catalogo

--- app/src/main/java/com/example/montes8/democatalogo/HomeActivity.kt
+++ app/src/main/java/com/example/montes8/democatalogo/HomeActivity.kt
@@ -2,11 +2,19 @@ package com.example.montes8.democatalogo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        ajusteToolbarHome()
+    }
+
+    private fun ajusteToolbarHome() {
+        setSupportActionBar(homeToolbar)
+        title = "CATEGORIAS"
     }
 }
