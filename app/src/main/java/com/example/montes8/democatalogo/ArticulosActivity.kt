c8981ab2d5c2ea9b67b328c4c5e3cac6cad015fd
montes8 <emontesinos812@gmail.com>
2018-07-26 20:29:02 -0500

    implementando menu

---
+++ app/src/main/java/com/example/montes8/democatalogo/ArticulosActivity.kt
@@ -0,0 +1,12 @@
+package com.example.montes8.democatalogo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ArticulosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_articulos)
+    }
+}
--- app/src/main/java/com/example/montes8/democatalogo/HomeActivity.kt
+++ app/src/main/java/com/example/montes8/democatalogo/HomeActivity.kt
@@ -1,7 +1,10 @@
 package com.example.montes8.democatalogo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -16,5 +19,20 @@ class HomeActivity : AppCompatActivity() {
     private fun ajusteToolbarHome() {
         setSupportActionBar(homeToolbar)
         title = "CATEGORIAS"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.articulos -> {
+                val intent = Intent(this@HomeActivity,ArticulosActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
